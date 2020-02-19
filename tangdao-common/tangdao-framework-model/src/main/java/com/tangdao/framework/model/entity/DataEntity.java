6b921c6c2d8e3b5cffff38ae527f87d0bc67c18e
ruyangit <ruyangit@163.com>
2020-02-19 18:12:26 +0800

    0.0.1 接口规范？

--- tangdao-common/tangdao-framework-model/src/main/java/com/tangdao/framework/model/entity/DataEntity.java
+++ tangdao-common/tangdao-framework-model/src/main/java/com/tangdao/framework/model/entity/DataEntity.java
@@ -54,4 +54,64 @@ public abstract class DataEntity extends BaseEntity {
 	public DataEntity() {
 		
 	}
+
+	public String getCreateBy() {
+		return createBy;
+	}
+
+
+	public void setCreateBy(String createBy) {
+		this.createBy = createBy;
+	}
+
+
+	public Date getCreateTime() {
+		return createTime;
+	}
+
+
+	public void setCreateTime(Date createTime) {
+		this.createTime = createTime;
+	}
+
+
+	public String getUpdateBy() {
+		return updateBy;
+	}
+
+
+	public void setUpdateBy(String updateBy) {
+		this.updateBy = updateBy;
+	}
+
+
+	public Date getUpdateTime() {
+		return updateTime;
+	}
+
+
+	public void setUpdateTime(Date updateTime) {
+		this.updateTime = updateTime;
+	}
+
+
+	public String getStatus() {
+		return status;
+	}
+
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+
+	public String getRemarks() {
+		return remarks;
+	}
+
+
+	public void setRemarks(String remarks) {
+		this.remarks = remarks;
+	}
+	
 }
--- tangdao-module-core-api/src/main/java/com/tangdao/module/core/controller/UserController.java
+++ tangdao-module-core-api/src/main/java/com/tangdao/module/core/controller/UserController.java
@@ -1,23 +1,41 @@
 package com.tangdao.module.core.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.tangdao.module.core.model.domain.User;
 import com.tangdao.module.core.service.IUserService;
 
 @RestController
-@RequestMapping(value = "users", produces="application/json;charset=utf-8")
+@RequestMapping(value = "/api/{version}/users",produces = MediaType.APPLICATION_JSON_VALUE)
 public class UserController {
 	
 	@Autowired
 	private IUserService userService;
 
-	@GetMapping("{user_code}")
-	public User get(@PathVariable("user_code") String userCode) {
-		return userService.getById(userCode);
+	@GetMapping
+	public IPage<User> page(Page<User> page, User user) {
+		QueryWrapper<User> queryWrapper = new QueryWrapper<User>();
+		return userService.page(page, queryWrapper);
+	}
+	
+	@GetMapping
+	public List<User> list(User user) {
+		QueryWrapper<User> queryWrapper = new QueryWrapper<User>();
+		return userService.list(queryWrapper);
+	}
+	
+	@GetMapping("{id}")
+	public User get(@PathVariable("id") String id) {
+		return userService.getById(id);
 	}
 }
