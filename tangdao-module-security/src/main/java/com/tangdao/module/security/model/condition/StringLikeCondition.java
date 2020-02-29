187e16085da9f0adf5b1a818e0e198a32c39a835
ruyangit <ruyangit@163.com>
2020-02-29 23:42:35 +0800

    0.2.6 字符条件

---
+++ tangdao-module-security/src/main/java/com/tangdao/module/security/model/condition/StringLikeCondition.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+package com.tangdao.module.security.model.condition;
+
+import java.util.regex.Pattern;
+
+import org.springframework.stereotype.Component;
+
+import com.tangdao.common.utils.StringUtils;
+
+/**
+ * <p>
+ * TODO 描述
+ * </p>
+ *
+ * @author ruyangit@gmail.com
+ * @since 2020年2月29日
+ */
+@Component
+public class StringLikeCondition extends Condition {
+
+	@Override
+	public boolean evaluate(String key, String value) {
+		// TODO Auto-generated method stub
+		String pattern = StringUtils.patternFromGlob(value);
+		return key == null || Pattern.matches(pattern, key);
+	}
+
+}
