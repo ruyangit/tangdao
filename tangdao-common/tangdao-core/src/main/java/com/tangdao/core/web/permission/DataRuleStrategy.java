1e9e949227a21e764964696d558afc6d282c83d3
ruyang <ruyangit@163.com>
2020-06-15 14:14:59 +0800

    数据权限

---
+++ tangdao-common/tangdao-core/src/main/java/com/tangdao/core/web/permission/DataRuleStrategy.java
@@ -0,0 +1,17 @@
+/**
+ *
+ */
+package com.tangdao.core.web.permission;
+
+/**
+ * <p>
+ * TODO 描述
+ * </p>
+ *
+ * @author ruyang@gmail.com
+ * @since 2020年6月15日
+ */
+public enum DataRuleStrategy {
+
+	TEXT, CHOICE, SYSTEM_PROVIDER
+}
