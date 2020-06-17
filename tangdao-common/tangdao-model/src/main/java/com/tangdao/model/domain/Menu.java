dbfa58076518aa37109c1bd1b73c3bc19b7eeaa9
ruyang <ruyangit@163.com>
2020-06-17 18:27:04 +0800

    权限提交

--- tangdao-common/tangdao-model/src/main/java/com/tangdao/model/domain/Menu.java
+++ tangdao-common/tangdao-model/src/main/java/com/tangdao/model/domain/Menu.java
@@ -4,7 +4,9 @@
 package com.tangdao.model.domain;
 
 import java.util.Date;
+import java.util.List;
 
+import com.baomidou.mybatisplus.annotation.TableField;
 import com.tangdao.model.base.BaseEntity;
 
 import lombok.Getter;
@@ -28,23 +30,23 @@ public class Menu extends BaseEntity {
 	private static final long serialVersionUID = 1L;
 
 	private String name;
-	
+
 	private String pid;
-	
+
 	private String path;
-	
+
 	private String premission;
-	
+
 	private String menuType;
-	
+
 	private Integer sort;
-	
+
 	private String icon;
-	
+
 	private String link;
-	
+
 	private String opened;
-	
+
 	private String badge;
 
 	private String remark;
@@ -52,4 +54,11 @@ public class Menu extends BaseEntity {
 	private String status;
 
 	private Date modified;
+
+	@TableField(exist = false)
+	private List<Menu> children;
+
+	public void addChild(Menu menu) {
+		this.children.add(menu);
+	}
 }
--- tangdao-service-provider/src/main/java/com/tangdao/modules/sys/service/MenuService.java
+++ tangdao-service-provider/src/main/java/com/tangdao/modules/sys/service/MenuService.java
@@ -3,11 +3,20 @@
  */
 package com.tangdao.modules.sys.service;
 
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.stereotype.Service;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.tangdao.core.constant.DataStatus;
 import com.tangdao.core.service.BaseService;
 import com.tangdao.model.domain.Menu;
 import com.tangdao.modules.sys.mapper.MenuMapper;
+
+import cn.hutool.core.util.StrUtil;
 
 /**
  * <p>
@@ -20,4 +29,37 @@ import com.tangdao.modules.sys.mapper.MenuMapper;
 @Service
 public class MenuService extends BaseService<MenuMapper, Menu> {
 
+	public List<Menu> findByNormal(QueryWrapper<Menu> queryWrapper) {
+		queryWrapper.eq("status", DataStatus.NORMAL);
+		queryWrapper.orderByAsc("sort", "id");
+		return this.list(queryWrapper);
+	}
+
+	public List<Menu> findChildList(List<Menu> sourceList, List<Menu> targetList, String pid) {
+		// 构建一个Map,把所有原始数据的ID作为Key,原始数据对象作为VALUE
+		Map<String, Menu> dtoMap = new LinkedHashMap<String, Menu>();
+		for (Menu tree : sourceList) {
+			
+			// 原始数据对象为Node，放入dtoMap中。
+			tree.setChildren(null);
+			dtoMap.put(tree.getId(), tree);
+		}
+
+		for (Map.Entry<String, Menu> entry : dtoMap.entrySet()) {
+			Menu menu = entry.getValue();
+			String tpid = menu.getPid();
+			if (dtoMap.get(tpid) == null) {
+				// 如果是顶层节点，直接添加到结果集合中
+				targetList.add(menu);
+			} else {
+				// 如果不是顶层节点，有父节点，然后添加到父节点的子节点中
+				Menu parent = dtoMap.get(tpid);
+				if (parent.getChildren() == null) {
+					parent.setChildren(new LinkedList<Menu>());
+				}
+				parent.addChild(menu);
+			}
+		}
+		return targetList;
+	}
 }
