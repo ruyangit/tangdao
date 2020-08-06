44ba3dd6fecbad61f1d01e009078f877d1e253ae
ruyang <ruyangit@163.com>
2020-08-06 15:48:19 +0800

    商品品牌

---
+++ module-shop/goods/goods-model/src/main/java/com/tangdao/modules/goods/model/domain/GoodsBrand.java
@@ -0,0 +1,30 @@
+/**
+ *
+ */
+package com.tangdao.modules.goods.model.domain;
+
+import java.io.Serializable;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * <p>
+ * TODO 描述
+ * </p>
+ *
+ * @author ruyang@gmail.com
+ * @since 2020年5月28日
+ */
+@Getter
+@Setter
+public class GoodsBrand implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	
+	
+}
