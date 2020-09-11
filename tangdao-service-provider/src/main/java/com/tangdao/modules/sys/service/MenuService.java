a04c72d3e54f334f8ca75b93805828becba91c93
ruyang <ruyangit@163.com>
2020-09-11 18:06:38 +0800

    权限模块修改

--- tangdao-service-provider/src/main/java/com/tangdao/modules/sys/service/MenuService.java
+++ tangdao-service-provider/src/main/java/com/tangdao/modules/sys/service/MenuService.java
@@ -9,7 +9,6 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
-import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
