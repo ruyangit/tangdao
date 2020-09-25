16125f93ceaa00861ee31e6e7654a5db33b50ac1
ruyang <ruyangit@163.com>
2020-09-25 13:15:58 +0800

    商品列表

--- module-shop/goods/goods-web/src/main/java/com/tangdao/modules/goods/controller/admin/GoodsController.java
+++ module-shop/goods/goods-web/src/main/java/com/tangdao/modules/goods/controller/admin/GoodsController.java
@@ -3,12 +3,28 @@
  */
 package com.tangdao.modules.goods.controller.admin;
 
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.tangdao.common.CommonResponse;
 import com.tangdao.core.web.BaseController;
+import com.tangdao.core.web.validate.Field;
+import com.tangdao.core.web.validate.Rule;
+import com.tangdao.core.web.validate.Validate;
+import com.tangdao.modules.goods.model.domain.Goods;
 import com.tangdao.modules.goods.service.GoodsBrandService;
+import com.tangdao.modules.goods.service.GoodsService;
+
+import cn.hutool.core.map.MapUtil;
+import cn.hutool.core.util.StrUtil;
 
 /**
  * <p>
@@ -24,41 +40,43 @@ public class GoodsController extends BaseController {
 
 	@Autowired
 	GoodsBrandService goodsBrandService;
+	
+	@Autowired
+	GoodsService goodsService;
 
-//	@GetMapping("/brands")
-//	public CommonResponse page(Page<GoodsBrand> page, String brandName) {
-//		QueryWrapper<GoodsBrand> queryWrapper = new QueryWrapper<GoodsBrand>();
-//		if (StrUtil.isNotBlank(brandName)) {
-//			queryWrapper.like("brand_name", brandName);
-//		}
-//		return success(goodsBrandService.page(page, queryWrapper));
-//	}
-//
-//	@Validate({ @Field(name = "id", rules = { @Rule(message = "主键不能为空") }) })
-//	@GetMapping("/brand-detail")
-//	public CommonResponse detail(String id) {
-//		GoodsBrand goodsBrand = goodsBrandService.getById(id);
-//		Map<String, Object> data = MapUtil.newHashMap();
-//		data.put("goodsBrand", goodsBrand);
-//		return success(data);
-//	}
-//
-//	@Validate({ @Field(name = "goodsBrand.brandName", rules = { @Rule(message = "角色名不能为空") }) })
-//	@PostMapping("/brands")
-//	public CommonResponse saveGoodsBrand(@RequestBody GoodsBrand goodsBrand) {
-//		return success(goodsBrandService.save(goodsBrand));
-//	}
-//
-//	@Validate({ @Field(name = "goodsBrand.id", rules = { @Rule(message = "主键不能为空") }) })
-//	@PostMapping("/brand-update")
-//	public CommonResponse updateRole(@RequestBody GoodsBrand goodsBrand) {
-//		goodsBrand.setEmpty();
-//		return success(goodsBrandService.updateById(goodsBrand));
-//	}
-//
-//	@Validate({ @Field(name = "goodsBrand.id", rules = { @Rule(message = "主键不能为空") }) })
-//	@PostMapping("/brand-delete")
-//	public CommonResponse deleteGoodsBrand(@RequestBody GoodsBrand goodsBrand) {
-//		return success(goodsBrandService.removeById(goodsBrand.getId()));
-//	}
+	@GetMapping
+	public CommonResponse page(Page<Goods> page, String goodsName) {
+		QueryWrapper<Goods> queryWrapper = new QueryWrapper<Goods>();
+		if (StrUtil.isNotBlank(goodsName)) {
+			queryWrapper.like("goods_name", goodsName);
+		}
+		return success(goodsService.page(page, queryWrapper));
+	}
+
+	@Validate({ @Field(name = "id", rules = { @Rule(message = "主键不能为空") }) })
+	@GetMapping("/detail")
+	public CommonResponse detail(String id) {
+		Goods goods = goodsService.getById(id);
+		Map<String, Object> data = MapUtil.newHashMap();
+		data.put("goods", goods);
+		return success(data);
+	}
+
+	@Validate({ @Field(name = "goods.goodsName", rules = { @Rule(message = "商品名称不能为空") }) })
+	@PostMapping
+	public CommonResponse saveGoods(@RequestBody Goods goods) {
+		return success(goodsService.save(goods));
+	}
+
+	@Validate({ @Field(name = "goods.id", rules = { @Rule(message = "主键不能为空") }) })
+	@PostMapping("/update")
+	public CommonResponse updateGoods(@RequestBody Goods goods) {
+		return success(goodsService.updateById(goods));
+	}
+
+	@Validate({ @Field(name = "goods.id", rules = { @Rule(message = "主键不能为空") }) })
+	@PostMapping("/delete")
+	public CommonResponse deleteBrand(@RequestBody Goods goods) {
+		return success(goodsService.removeById(goods.getId()));
+	}
 }
