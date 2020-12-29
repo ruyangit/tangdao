9d8a570bb28d688218c9d1ec30379a5ae6753f6c
ruyang <ruyangit@163.com>
2020-12-29 10:10:27 +0800

    base

---
+++ tangdao-core/src/main/java/com/tangdao/core/model/Menu.java
@@ -0,0 +1,23 @@
+/**
+ *
+ */
+package com.tangdao.core.model;
+
+import com.tangdao.common.BaseModel;
+
+/**
+ * <p>
+ * TODO 菜单
+ * </p>
+ *
+ * @author ruyang
+ * @since 2020年12月29日
+ */
+public class Menu extends BaseModel {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
