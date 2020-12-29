b17fa19046ccf73604fc04af32e9a6b8b2f2a2ad
ruyang <ruyangit@163.com>
2020-12-29 15:03:53 +0800

    2.0.4

---
+++ tangdao-service/src/main/java/com/tangdao/modules/sys/service/DictService.java
@@ -0,0 +1,19 @@
+/**
+ *
+ */
+package com.tangdao.modules.sys.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * <p>
+ * TODO 描述
+ * </p>
+ *
+ * @author ruyang
+ * @since 2020年12月29日
+ */
+@Service
+public class DictService {
+
+}
