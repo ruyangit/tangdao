836713561a333ddcf5a46772a4fbba48ba3a6185
ruyang <ruyangit@163.com>
2021-01-05 11:25:54 +0800

    dict

---
+++ tangdao-service/src/main/java/com/tangdao/modules/sys/service/DictTypeService.java
@@ -0,0 +1,23 @@
+/**
+ *
+ */
+package com.tangdao.modules.sys.service;
+
+import org.springframework.stereotype.Service;
+
+import com.tangdao.core.service.BaseService;
+import com.tangdao.model.domain.DictType;
+import com.tangdao.modules.sys.dao.DictTypeMapper;
+
+/**
+ * <p>
+ * TODO 描述
+ * </p>
+ *
+ * @author ruyang
+ * @since 2020年12月29日
+ */
+@Service
+public class DictTypeService extends BaseService<DictTypeMapper, DictType>{
+
+}
