947654527cf6119d9ba08462b0d8b891861713ea
ruyang <ruyangit@163.com>
2021-02-23 18:15:21 +0800

    tc

--- tangdao-core/src/main/java/com/tangdao/core/context/TaskContext.java
+++ tangdao-core/src/main/java/com/tangdao/core/context/TaskContext.java
@@ -35,15 +35,15 @@ public class TaskContext {
 		public static PacketsProcessStatus parse(int code) {
 			for (PacketsProcessStatus pt : PacketsProcessStatus.values()) {
 				if (pt.getCode() == code) {
-                    {
-                        return pt;
-                    }
-                }
+					{
+						return pt;
+					}
+				}
 			}
 			return PROCESS_EXCEPTION;
 		}
 	}
-	
+
 	public enum PacketsApproveStatus {
 		WAITING(0, "待审核"), AUTO_COMPLETE(1, "自动通过"), HANDLING_COMPLETE(2, "手动通过"), REJECT(3, "驳回");
 
@@ -66,15 +66,15 @@ public class TaskContext {
 		public static PacketsApproveStatus parse(int code) {
 			for (PacketsApproveStatus pt : PacketsApproveStatus.values()) {
 				if (pt.getCode() == code) {
-                    {
-                        return pt;
-                    }
-                }
+					{
+						return pt;
+					}
+				}
 			}
 			return WAITING;
 		}
 	}
-	
+
 	public enum MessageSubmitStatus {
 		SUCCESS(0, "处理成功"), FAILED(1, "处理失败");
 
@@ -97,15 +97,15 @@ public class TaskContext {
 		public static MessageSubmitStatus parse(int code) {
 			for (MessageSubmitStatus pt : MessageSubmitStatus.values()) {
 				if (pt.getCode() == code) {
-                    {
-                        return pt;
-                    }
-                }
+					{
+						return pt;
+					}
+				}
 			}
 			return FAILED;
 		}
 	}
-	
+
 	public enum PacketsActionPosition {
 		SMS_TEMPLATE_MISSED(0, "短信模板未报备"), FOBIDDEN_WORDS(1, "短信存在敏感词"), PASSAGE_NOT_AVAIABLE(2, "通道不可用");
 
@@ -134,9 +134,10 @@ public class TaskContext {
 		}
 
 	}
-	
+
 	public enum PacketsActionActor {
 		AVAIABLE('0', "可用"), BROKEN('1', "故障");
+
 		private char actor;
 		private String title;
 
@@ -162,10 +163,11 @@ public class TaskContext {
 		}
 
 	}
-	
+
 	public enum TaskSubmitType {
 
 		BATCH_MESSAGE(0, "批量"), POINT_TO_POINT(1, "普通点对点"), TEMPLATE_POINT_TO_POINT(2, "模板点对点");
+
 		private int code;
 		private String title;
 
