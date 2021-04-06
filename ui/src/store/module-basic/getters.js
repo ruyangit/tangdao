e492a6eb469e9ca2999531c5d68fc60727ebdb97
ruyang <ruyangit@163.com>
2021-04-06 11:44:12 +0800

    ~~

--- ui/src/store/module-basic/getters.js
+++
@@ -1,6 +0,0 @@
-export const leftDrawerOpen = (state) => {
-  return state.leftDrawerOpen
-}
-export const globalLoading = (state) => {
-  return state.global.loading
-}
--- ui/src/store/module-basic/mutations.js
+++
@@ -1,6 +0,0 @@
-export const leftDrawerOpenMutAction = (state, leftDrawerOpen) => {
-  state.leftDrawerOpen = leftDrawerOpen
-}
-export const globalLoadingMutAction = (state, loading) => {
-  state.global.loading = loading
-}
