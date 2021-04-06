3ec7115965974df367154c6b15d48a49c7a61f20
ruyangit <ruyangit@163.com>
2021-04-06 12:06:44 +0800

    ~~

--- tangdao-web/bin/run-tomcat.sh
+++ tangdao-web/bin/run-tomcat.sh
@@ -1,9 +1,6 @@
 #!/bin/sh
-
 echo ""
 echo "[信息] 使用 Spring Boot Tomcat 运行 Web 工程。"
 echo ""
-
 cd ..
-MAVEN_OPTS="$MAVEN_OPTS -Xms256m -Xmx1024m -XX:MetaspaceSize=128m -XX:MaxMetaspaceSize=512m"
 mvn clean spring-boot:run -Dmaven.test.skip=true -U
