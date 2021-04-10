aaddde6167eec92e0734387eb9345ed974214411
ruyangit <ruyangit@163.com>
2021-04-10 13:49:26 +0800

    ~~

--- core/src/main/java/com/tangdao/core/service/BaseService.java
+++ core/src/main/java/com/tangdao/core/service/BaseService.java
@@ -5,8 +5,12 @@ package com.tangdao.core.service;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.baomidou.mybatisplus.core.conditions.update.UpdateWrapper;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.baomidou.mybatisplus.core.metadata.TableInfo;
+import com.baomidou.mybatisplus.core.metadata.TableInfoHelper;
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 
@@ -22,18 +26,19 @@ public abstract class BaseService<M extends BaseMapper<T>, T> extends ServiceImp
 
 	protected Logger logger = LoggerFactory.getLogger(getClass());
 
-//	/**
-//	 * 
-//	 * TODO 状态更新
-//	 * @param pkVal  主鍵值
-//	 * @param status 更新状态
-//	 * @return
-//	 */
-//	@Transactional(rollbackFor = Exception.class)
-//	public boolean updateStatus(String pkVal, String status) {
-//		UpdateWrapper<T> updateWrapper = new UpdateWrapper<T>();
-//		updateWrapper.set("status", status);
-//		updateWrapper.eq(TableTreeColumnScannerRegistrar.getPrimaryKey(getEntityClass()), pkVal);
-//		return super.update(updateWrapper);
-//	}
+	/**
+	 * 
+	 * TODO 状态更新
+	 * @param pkVal  主鍵值
+	 * @param status 更新状态
+	 * @return
+	 */
+	@Transactional(rollbackFor = Exception.class)
+	public boolean updateStatus(String pkVal, String status) {
+		UpdateWrapper<T> updateWrapper = new UpdateWrapper<T>();
+		updateWrapper.set("status", status);
+		TableInfo tableInfo = TableInfoHelper.getTableInfo(getEntityClass());
+		updateWrapper.eq(tableInfo.getKeyColumn(), pkVal);
+		return super.update(updateWrapper);
+	}
 }
