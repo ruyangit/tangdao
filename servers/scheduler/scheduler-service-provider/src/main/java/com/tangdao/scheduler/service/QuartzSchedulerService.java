b4041f4c59203c58e8d3b93cc3c8fbab46e07f5c
ruyang <ruyangit@163.com>
2021-04-13 12:51:07 +0800

    ~~

---
+++ servers/scheduler/scheduler-service-provider/src/main/java/com/tangdao/scheduler/service/QuartzSchedulerService.java
@@ -0,0 +1,19 @@
+/**
+ *
+ */
+package com.tangdao.scheduler.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * <p>
+ * TODO 描述
+ * </p>
+ *
+ * @author ruyang
+ * @since 2021年4月13日
+ */
+@Service
+public class QuartzSchedulerService {
+
+}
