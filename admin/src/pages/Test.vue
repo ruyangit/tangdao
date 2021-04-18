d7179b1129e97b90ff8fc8a1cf8f9576117747cd
ruyangit <ruyangit@163.com>
2021-04-18 23:56:02 +0800

    ~~

--- admin/src/pages/Test.vue
+++ admin/src/pages/Test.vue
@@ -1,7 +1,27 @@
 <template>
   <div class="q-pa-md">
-    <!-- {{retData}} -->
-    <q-markup-table class="my-table">
+    {{retData}}
+    <q-card>
+      <q-card-section class="q-gutter-xs">
+        <q-btn
+          label="展开"
+          color="primary"
+          @click="onExpand"
+        />
+        <q-btn
+          label="折叠"
+          color="primary"
+          @click="onCollapse"
+        />
+        <q-btn
+          label="刷新"
+          color="primary"
+          @click="onRefresh"
+        />
+      </q-card-section>
+    </q-card>
+    <q-markup-table class="my-table q-mt-md">
+      <!-- <q-linear-progress indeterminate /> -->
       <thead>
         <tr>
           <th class="text-left wd-120">Dessert (100g serving)</th>
@@ -47,6 +67,7 @@ export default {
   },
   data () {
     return {
+      loading: false,
       data: [
         { id: '1', pid: '0', name: 'test-1', avg: 122 },
         { id: '1-1', pid: '1', name: 'test-1-1', avg: 312 },
@@ -70,7 +91,13 @@ export default {
     this.retData = this.onRequestData({ id: '0' })
   },
   methods: {
+    onRefresh () {
+    },
     onRequestData (node) {
+      this.loading = true
+      setTimeout(() => {
+        this.loading = false
+      }, 1000)
       return this.data.filter(item => { return item.pid === node.id })
     },
     expandRow (node, index) {
@@ -108,6 +135,53 @@ export default {
           travel(item)
         })
       }
+    },
+    onExpand () {
+      this.retData && this.retData.forEach(item => {
+        if (!item.children) {
+          const children = this.onRequestData(item)
+          if (children && children.length > 0) {
+            this.$set(item, 'children', children)
+          }
+        }
+      })
+      console.log(this.retData)
+      const _retData = []
+      // const travel = (item) => {
+      //   item.children && item.children.forEach(ele => {
+      //     ele.level = item.level + 1
+      //     _retData.push(ele)
+      //     if (ele.children && ele.children.length > 0) {
+      //       travel(ele)
+      //     }
+      //   })
+      // }
+      this.retData && this.retData.forEach(item => {
+        item.level = 1
+        _retData.push(item)
+        if (item.children && item.children.length > 0) {
+          // travel(item)
+        }
+      })
+      console.log(_retData)
+      this.retData = _retData
+    },
+    onCollapse () {
+      const travel = (data) => {
+        data && data.forEach(ele => {
+          ele.show = false
+          document.getElementById(ele.id).style = 'display: none'
+          if (ele.children && ele.children.length > 0) {
+            travel(ele.children)
+          }
+        })
+      }
+      this.retData && this.retData.forEach(item => {
+        item.show = false
+        if (item.children && item.children.length > 0) {
+          travel(item.children)
+        }
+      })
     }
   }
 }
